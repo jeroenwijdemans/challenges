a3c725b9165e3fcd4293adc2abd27f5e73b9a68e
Jeroen Wijdemans <jeroenwijdemans@gmail.com>
2013-07-02 21:08:50 +0200

    added 3 new scala Euler classes to implement

---
+++ scala/src/main/scala/nl/wijdemans/Euler09.scala
@@ -0,0 +1,23 @@
+package nl.wijdemans
+
+/**
+ *
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a2 + b2 = c2
+
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+
+ */
+object Euler09 {
+  def main(args: Array[String]) {
+    run()
+  }
+
+  def run() {
+
+  }
+}
---
+++ scala/src/main/scala/nl/wijdemans/Euler10.scala
@@ -0,0 +1,17 @@
+package nl.wijdemans
+
+/**
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+
+ */
+object Euler10 {
+  def main(args: Array[String]) {
+    run()
+  }
+
+  def run() {
+
+  }
+}
