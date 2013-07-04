df49199ae1e177453bbff78c7e37984e13482dd1
Jeroen Wijdemans <jeroenwijdemans@gmail.com>
2013-07-04 17:40:38 +0200

    eauler 16 summing it up ...

--- scala/src/main/scala/nl/wijdemans/Euler16.scala
+++ scala/src/main/scala/nl/wijdemans/Euler16.scala
@@ -1,5 +1,7 @@
 package nl.wijdemans
 
+import com.sun.tools.corba.se.idl.StringGen
+
 /**
 
 
@@ -10,6 +12,22 @@ What is the sum of the digits of the number 21000?
   */
 object Euler16 {
   def main(args: Array[String]) {
+    var a: java.math.BigDecimal = new java.math.BigDecimal(1)
+    val TWO = new java.math.BigDecimal(2)
+    for (i <- 1 to 1000) {
+      a = a.multiply(TWO)
+    }
+    var tot = a.toPlainString();
+    println("BD : " + tot);
+
+    var x = 0
+
+    tot.foreach {
+      case ch => x += Integer.valueOf(ch.toString())
+    }
+
+    // 1366
+    println("Sum : " + x)
 
   }
 }
